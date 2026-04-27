6851e019bf3c5dd2d5313a6eb2792d5ec59e7721
NaeemaNishad <naeema.nishad10@gmail.com>
2026-04-27 14:56:15 +0530

    feat: implement modular config, responsive UI, and MQTT integration

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // Replace with your ThingSpeak Channel ID
+    THING_SPEAK_ID: "3186168", 
+    // Replace with your ThingSpeak Read API Key
+    READ_API_KEY: "FJ7LCU6WK5ZA3BTR", 
+    // Your unique MQTT prefix
+    MQTT_PREFIX: "naeema_igdtuw_secure/", 
+    // Your Local ESP32-CAM IP (e.g., 192.168.1.50)
+    CAMERA_IP: "10.152.46.58", 
+};
